e99b3fa3534b2a4391240b31d14285c69558067c
Vipin-88 <vipin.kumar@knowledgeexcel.com>
2026-04-17 14:19:03 +0530

    base is updated from /enspeek/ to /

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/enspeek/',
+  // base:'/enspeek/',
+  base:'/',
   plugins: [react(), tailwindcss()],
 })
