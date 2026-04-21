dcda07b62f65331f0001422355951fc8ea0f4bd3
Vipin-88 <vipin.kumar@knowledgeexcel.com>
2026-04-21 18:21:42 +0530

    `Refactor login form and layout to match new design`

--- src/layout/Auth.layout.tsx
+++ src/layout/Auth.layout.tsx
@@ -1,19 +1,11 @@
 import { Outlet } from 'react-router'
-import Background from '../components/common/Auth/Background'
-import { ColoredLogo } from '../assets/icons'
 
 const Auth_layout = () => {
   return (
-    <div className='min-h-screen w-full flex items-center justify-center bg-white p-2'>
-        <div className='absolute top-0 left-2'>
-            <img src={ColoredLogo} alt='Insights Curry' className='w-[70%]'/>
-        </div>
-        <div className='flex w-full h-full items-center justify-center'>
-            <div className='w-full max-w-4xl overflow-hidden rounded-2xl flex bg-white shadow-md border border-gray-200'>
-                <Background/>
-                <Outlet/>
-            </div>
-        </div>
+    <div className='min-h-dvh w-full overflow-y-auto bg-gradient-to-br from-login-bg-start to-login-bg-end px-4 py-4 sm:px-6 sm:py-6'>
+      <div className='mx-auto flex min-h-[calc(100dvh-2rem)] w-full max-w-7xl items-center justify-center sm:min-h-[calc(100dvh-3rem)]'>
+        <Outlet />
+      </div>
     </div>
   )
 }
